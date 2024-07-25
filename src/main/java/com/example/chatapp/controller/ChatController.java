d5125da2a0697e4e3122efa5f500696b3fe79110
devopcls <devops0124@gmail.com>
2024-07-25 18:51:24 +0530

    Added the stucture for the project

---
+++ src/main/java/com/example/chatapp/controller/ChatController.java
@@ -0,0 +1,17 @@
+package com.example.chatapp.controller;
+
+import com.example.chatapp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
+}
+
---
+++ src/main/java/com/example/chatapp/model/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.example.chatapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    private String content;
+    private String sender;
+}
+
